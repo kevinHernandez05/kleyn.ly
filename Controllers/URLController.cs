2f435fd8711ea9b71980b43bb233d3547d24cea4
Kevin Hernandez <kevinhdez@Kevins-MacBook-Pro.local>
2019-05-23 14:13:41 -0400

    Inicio del proyecto

---
+++ Controllers/URLController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kleyn.ly.Controllers
+{
+    public class URLController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
